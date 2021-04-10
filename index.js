5687f106b78814f0cc817643afa4fba8658afb6c
Mukulbaid63 <mukulbaid63@gmail.com>
2021-04-11 01:41:22 +0545

    Final improvements

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+const WebSocket = require("ws");
 
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port:process.env.PORT|| 8080 });
-var connectCounter=0;
-wss.on('connection', function connection(ws) {
-  console.log("CONNECTED")
-    connectCounter++;
-    ws.on('close',()=>{
-      connectCounter--;
-      console.log(connectCounter)
-    })
-    if(connectCounter<=5){
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-        
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-     
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+var connectCounter = 0;
+wss.on("connection", function connection(ws) {
+  console.log("CONNECTED");
+  connectCounter++;
+  ws.on("close", () => {
+    connectCounter--;
+    console.log(connectCounter);
+  });
+  if (connectCounter <= 5) {
+    ws.on("message", function incoming(data) {
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    });
+  } else {
+    ws.send(
+      "MAX LIMIT REACHED"
+    );
+  }
 });
-  })
-}
-  else{
-    ws.send("The room is full, you are not allowed to chat but you can see the messages.")
-};
-});
-
